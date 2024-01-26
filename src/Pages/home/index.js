343addf22a01efd24cb7b74709780068ec8f988e
FilipeFRSantana <lipecleo@hotmail.com>
2024-01-26 16:31:18 -0300

    feat: Modified files JS, CSS and HTML

--- src/Pages/home/index.js
+++ src/Pages/home/index.js
@@ -1,6 +1,6 @@
 const renderFinancesList = (data) => {
   const table = document.getElementById("finances-table");
-  
+
   data.map((item) => {
     const tableRow = document.createElement("tr");
     tableRow.className = "mt smaller";
@@ -156,7 +156,38 @@ const onLoadUserInfo = () => {
   navbarUserAvatar.appendChild(nameElement);
 };
 
+const onLoadCategories = async () => {
+  try {
+    const categoriesSelect = document.getElementById("input-category");
+    const response = await fetch(
+      `https://mp-wallet-app-api.herokuapp.com/categories`
+    );
+    const categoriesResult = await response.json();
+    categoriesResult.map((category) => {
+      const option = document.createElement("option");
+      const categoryText = document.createTextNode(category.name);
+      option.id = `category_${category.id}`;
+      option.value = category.id;
+      option.appendChild(categoryText);
+      categoriesSelect.append(option)
+    })
+  } catch (error) {
+    alert("Error em encontrar categoria");
+  }
+};
+
+const onOpenModal = () => {
+  const modal = document.getElementById("modal");
+  modal.style.display = "flex";
+};
+
+const onCloseModal = () => {
+  const modal = document.getElementById("modal");
+  modal.style.display = "none";
+};
+
 window.onload = () => {
   onLoadUserInfo();
   onLoadFinancesData();
+  onLoadCategories();
 };
